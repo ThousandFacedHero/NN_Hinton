2912748906dd9ff65c668a806650556dc8cdbdd3
ThousandFacedHero <mattlowe7@gmail.com>
2017-06-06 18:16:43 -0400

    almost

--- assignment3/a3.m
+++ assignment3/a3.m
@@ -129,9 +129,9 @@ function ret = loss(model, data, wd_coefficient)
   class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
   log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
   class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
-  
-  classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
-  wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
+  classification_loss = -sum(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
+  wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient;
+   % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
   ret = classification_loss + wd_loss;
 end
 
@@ -155,8 +155,7 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % This is the only function that you're expected to change. 
   % Right now, it just returns a lot of zeros, which is obviously 
    % not the correct output. Your job is to replace that by a correct computation.
-  ret.input_to_hid = model.input_to_hid * 0;
-  ret.hid_to_class = model.hid_to_class * 0;
+
   
   hid_input = model.input_to_hid * data.inputs;
   hid_output = logistic(hid_input);
@@ -164,19 +163,16 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   class_normalizer = log_sum_exp_over_rows(class_input);
   log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]);
   class_prob = exp(log_class_prob);
+  classification_loss = -sum(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
+  wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient;
   
-  d3 = class_prob-data.targets;
-  d2 = model.hid_to_class'*d3.*((hid_output).*(1-hid_output));
-  ret.input_to_hid = d2*data.inputs;
-  ret.hid_to_class = d3*hid_output';
-  disp(size(model.input_to_hid));
-  disp(size(data.inputs));
-  disp(size(d2));
-  disp(size(ret.input_to_hid));
-  disp(size(model.hid_to_class));
-  disp(size(data.targets));
-  disp(size(d3));
-  disp(size(ret.hid_to_class));
+  ret.input_to_hid = ((((class_prob - data.targets)'*model.hid_to_class) * ...
+    (hid_output.*(1-hid_output)) * data.inputs')/size(data.targets, 2)) ...
+      +(classification_loss/size(data.targets, 2));
+    
+  ret.hid_to_class = (((class_prob - data.targets) * hid_output') / ...
+    (size(data.targets, 2))) +(wd_loss/size(data.targets, 2));
+
   
   
 end
